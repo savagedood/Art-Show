803d8db4473b06bcd14fe26af4b1cee2502c0ed6
savagedood <76217053+savagedood@users.noreply.github.com>
2021-01-24 12:52:34 -0500

    Update Obstacle.js
    added new velocity for the harder difficulty

--- Obstacle.js
+++ Obstacle.js
@@ -4,11 +4,18 @@ class Obstacle {
     this.vel = createVector(-0.25, 0);
     this.acc = createVector(0, 0);
     this.quality = good;
+   
   }
 
   show() {
     noStroke();
-    fill(random(255));
+     if (currentScene === 3) {
+      fill(random(255));
+    }
+    if (currentScene === 4) {
+      fill(0)
+      this.vel = createVector(-1, 0);
+    }
     // changing these numbers makes the collision not work, why?
     rect(this.pos.x, this.pos.y, 10, 10) // try changing the numbers in person collision too
     
@@ -25,11 +32,12 @@ class Special extends Obstacle {
   constructor() {
     super();
      this.quality = star;
+
   }
 
   show() {
     fill(255, 165, 0);
     ellipse(this.pos.x, this.pos.y, 20, 20)
-    
+
   }
 }
